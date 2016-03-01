ef8b6d57741da580267f464710f3d2fe8da7a657
Phillip Chiu <phillipychiu@gmail.com>
2016-03-01 12:07:10 -0800

    Fixed an issue with waypoint movement in the local reference frame.

--- Assets/Scripts/DataManagement/MovementPatternDataManager.cs
+++ Assets/Scripts/DataManagement/MovementPatternDataManager.cs
@@ -16,14 +16,24 @@ public class MovementPatternDataManager : MonoBehaviour {
         pattern.MovementQueue = new Queue<MovementAction>();
 
         WaypointMovementAction waypointAction1 = new WaypointMovementAction(new Vector2(5, 0), new List<Vector2>() { new Vector2(-2, 5), new Vector2(2, -5)}, 3, "Local");
-        WaypointMovementAction waypointAction2 = new WaypointMovementAction(new Vector2(0, 1), new List<Vector2>(), 1, "World");
+        WaypointMovementAction waypointAction2 = new WaypointMovementAction(new Vector2(10, 0), new List<Vector2>() { new Vector2(2.5f, 5), new Vector2(7.5f, -5)}, 3, "Local");
         VectorMovementAction vectorAction1 = new VectorMovementAction(0, 2, 0, 2, "Local");
         VectorMovementAction vectorAction2 = new VectorMovementAction(135, 2, 0, 2, "World");
 
         //pattern.MovementQueue.Enqueue(waypointAction1);
-        //pattern.MovementQueue.Enqueue(waypointAction2);
-        pattern.MovementQueue.Enqueue(vectorAction1);
-        pattern.MovementQueue.Enqueue(vectorAction2);
+        pattern.MovementQueue.Enqueue(waypointAction2);
+        //pattern.MovementQueue.Enqueue(vectorAction1);
+        //pattern.MovementQueue.Enqueue(vectorAction2);
         MovementPatterns.Add("TestPattern", pattern);
+    }
+
+    public void CreateTestShotPattern()
+    {
+        MovementPattern pattern = new MovementPattern();
+        pattern.MovementQueue = new Queue<MovementAction>();
+        WaypointMovementAction waypointAction1 = new WaypointMovementAction(new Vector2(0, 10), new List<Vector2>() { new Vector2(-5, 2.5f), new Vector2(5, 7.5f) }, 3, "Local");
+
+        pattern.MovementQueue.Enqueue(waypointAction1);
+        MovementPatterns.Add("TestShotPattern", pattern);
     }
 }
--- Assets/Scripts/Ships/Enemy.cs
+++ Assets/Scripts/Ships/Enemy.cs
@@ -6,8 +6,8 @@ public class Enemy : Ship {
 
     public MovementPattern MovementPattern;
     private float ElapsedTimeFraction;
+    private Queue<MovementAction> MovementQueue;
     private MovementAction CurrentAction;
-    private Vector2 StartPosition, EndPosition;
     private List<Vector2> CurrentWaypoints;
     private Vector3 CurrentDirection;
     private float ElapsedMovementTime;
@@ -15,7 +15,6 @@ public class Enemy : Ship {
 
 	// Use this for initialization
 	void Start () {
-        StartPosition = transform.position;
         ElapsedMovementTime = 0f;
         CurrentAction = null;
         CurrentWaypoints = new List<Vector2>();
@@ -40,13 +39,14 @@ public class Enemy : Ship {
     {
         if (MovementPattern != null)
         {
+            MovementQueue = new Queue<MovementAction>(MovementPattern.MovementQueue);
             SetCurrentAction();
         }
         while (CurrentAction != null)
         {
             if (ElapsedMovementTime > CurrentAction.Time)
             {
-                if (MovementPattern.MovementQueue.Count == 0)
+                if (MovementQueue.Count == 0)
                 {
                     CurrentAction = null;
                     continue;
@@ -60,7 +60,7 @@ public class Enemy : Ship {
             {
                 var WaypointAction = (WaypointMovementAction)CurrentAction;
                 ElapsedTimeFraction = ElapsedMovementTime / WaypointAction.Time;
-                transform.position = CalculateCurvePosition(CurrentWaypoints, ElapsedTimeFraction);
+                transform.position = Utilities.CalculateCurvePosition(CurrentWaypoints, ElapsedTimeFraction);
             }
             else
             {
@@ -73,7 +73,7 @@ public class Enemy : Ship {
 
     public void SetCurrentAction()
     {
-        CurrentAction = MovementPattern.MovementQueue.Dequeue();
+        CurrentAction = MovementQueue.Dequeue();
         if (CurrentAction.GetType().ToString() == "WaypointMovementAction")
         {
             var WaypointAction = (WaypointMovementAction)CurrentAction;
@@ -84,11 +84,11 @@ public class Enemy : Ship {
                 Vector3 direction = Vector2.zero;
                 foreach (Vector2 controlPoint in WaypointAction.ControlPoints)
                 {
-                    direction = transform.InverseTransformDirection(new Vector3(controlPoint.x, controlPoint.y) - transform.position);
+                    direction = transform.InverseTransformDirection(controlPoint);
                     CurrentWaypoints.Add(transform.position + direction);
                 }
 
-                direction = transform.InverseTransformDirection(new Vector3(WaypointAction.Origin.x, WaypointAction.Origin.y) - transform.position);
+                direction = transform.InverseTransformDirection(WaypointAction.Origin);
                 CurrentWaypoints.Add(transform.position + direction);
             }
             else
@@ -111,20 +111,5 @@ public class Enemy : Ship {
             }
         }
         ElapsedMovementTime = 0;
-    }
-
-    public Vector2 CalculateCurvePosition(List<Vector2> points, float fraction)
-    {
-        if (points.Count == 2)
-        {
-            return Vector2.Lerp(points[0], points[1], fraction);
-        }
-        List<Vector2> newPoints = new List<Vector2>();
-        for (int i = 0; i < points.Count - 1; i++)
-        {
-            newPoints.Add(Vector2.Lerp(points[i], points[i + 1], fraction));
-        }
-        return CalculateCurvePosition(newPoints, fraction);
-
     }
 }
